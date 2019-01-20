ee40dd7ed2ad948d3550f1136c58b596fa617af1
Zahar Tevs <tevsz2@gmail.com>
2019-01-21 02:40:15 +0500

    quite nice already

---
+++ dependencies.sh
@@ -0,0 +1,1 @@
+sudo pacman -S xf86-video-intel xf86-video-ati mesa noto-fonts rxvt-unicode dmenu xorg-server xorg-xinit xorg-xbacklight i3 feh chromium emacs neovim alsa-utils pulseaudio pulseaudio-alsa lm_sensors

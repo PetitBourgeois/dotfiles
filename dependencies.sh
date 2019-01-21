abf5e91e3c44db609f2faa8eb69b0bb63f97a79e
Zahar Tevs <tevsz2@gmail.com>
2019-01-21 21:20:31 +0500

    urxvt sucks

--- dependencies.sh
+++ dependencies.sh
@@ -1,1 +1,1 @@
-sudo pacman -S xf86-video-intel xf86-video-ati mesa noto-fonts rxvt-unicode dmenu xorg-server xorg-xinit xorg-xbacklight xorg-xinput i3 feh chromium emacs neovim alsa-utils pulseaudio pulseaudio-alsa lm_sensors ranger
+sudo pacman -S xf86-video-intel xf86-video-ati mesa noto-fonts rxvt-unicode dmenu xorg-server xorg-xinit xorg-xbacklight xorg-xinput i3 feh chromium emacs neovim alsa-utils pulseaudio pulseaudio-alsa lm_sensors ranger ttf-hack
